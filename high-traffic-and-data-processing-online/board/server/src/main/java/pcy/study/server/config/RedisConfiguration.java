a52f7572c8e76e7d63706f5c6928778ae70b4a60
cyPark95 <parkcy1007@gmail.com>
2026-02-17 18:09:17 +0900

    chore: Spring Cache - Redis 설정

---
+++ high-traffic-and-data-processing-online/board/server/src/main/java/pcy/study/server/config/RedisConfiguration.java
@@ -0,0 +1,66 @@
+package pcy.study.server.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfiguration {
+
+    private final String host;
+    private final int port;
+    private final String password;
+    private final long expiredTime;
+
+    public RedisConfiguration(
+            @Value("${spring.data.redis.host}")
+            String host,
+            @Value("${spring.data.redis.port}")
+            int port,
+            @Value("${spring.data.redis.password}")
+            String password,
+            @Value("${spring.data.redis.expired.defaultTime}")
+            long expiredTime
+    ) {
+        this.host = host;
+        this.port = port;
+        this.password = password;
+        this.expiredTime = expiredTime;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofSeconds(expiredTime))
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
